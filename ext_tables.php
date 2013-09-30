a5a603fdb982b526fe6fa056869f2bab40bbe33b
Piseth Sok <piseth@web-essentials.asia>
2013-09-30 11:45:03 +0700

    remove block code not used.

--- ext_tables.php
+++ ext_tables.php
@@ -15,33 +15,4 @@ $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'WE Map');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_wemap_domain_model_map', 'EXT:we_map/Resources/Private/Language/locallang_csh_tx_wemap_domain_model_map.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_wemap_domain_model_map');
-$TCA['tx_wemap_domain_model_map'] = array(
-	'ctrl' => array(
-		'title'	=> 'LLL:EXT:we_map/Resources/Private/Language/locallang_db.xlf:tx_wemap_domain_model_map',
-		'label' => 'name',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'dividers2tabs' => TRUE,
-		'sortby' => 'sorting',
-		'versioningWS' => 2,
-		'versioning_followPages' => TRUE,
-		'origUid' => 't3_origuid',
-		'languageField' => 'sys_language_uid',
-		'transOrigPointerField' => 'l10n_parent',
-		'transOrigDiffSourceField' => 'l10n_diffsource',
-		'delete' => 'deleted',
-		'enablecolumns' => array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-		),
-		'searchFields' => 'name,',
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Map.php',
-		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_wemap_domain_model_map.gif'
-	),
-);
-
 ?>
